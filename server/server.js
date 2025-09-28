91d2a433684d93b1aa2df1915691d70901cc29af
jignesh <jigneshramawat21@gmail.com>
2025-09-28 16:59:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 4000
 connectDB();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://arp21.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
